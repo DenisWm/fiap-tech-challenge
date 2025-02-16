6fb6431682a93ad9bd96cf887aa2125229f1fb34
DenisWm <denismamoni@hotmail.com>
2025-02-16 18:53:46 -0300

    as

--- src/test/java/com/fiap/tech/product/infra/api/ProductAPITest.java
+++ src/test/java/com/fiap/tech/product/infra/api/ProductAPITest.java
@@ -10,7 +10,10 @@ import com.fiap.tech.product.application.delete.DefaultDeleteProductUseCase;
 import com.fiap.tech.product.application.retrieve.get.DefaultGetProductByIdUseCase;
 import com.fiap.tech.product.application.retrieve.list.DefaultListProductsUseCase;
 import com.fiap.tech.product.application.update.DefaultUpdateProductUseCase;
+import com.fiap.tech.product.application.update.UpdateProductCommand;
+import com.fiap.tech.product.application.update.UpdateProductOutput;
 import com.fiap.tech.product.infra.models.CreateProductRequest;
+import com.fiap.tech.product.infra.models.UpdateProductRequest;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +29,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -78,6 +82,38 @@ class ProductAPITest {
         final var captor = ArgumentCaptor.forClass(CreateProductCommand.class);
 
         verify(createProductUseCase).execute(captor.capture());
+
+        final var aCmd = captor.getValue();
+
+        assertEquals(expectedName, aCmd.name());
+        assertEquals(expectedDescription, aCmd.description());
+        assertEquals(expectedPrice, aCmd.price());
+    }
+
+    @Test
+    void givenValidRequest_whenUpdateProduct_thenReturnIdAndStatus200() throws Exception {
+        final var expectedName = "Test Product Name";
+        final var expectedDescription = "Test Product Description";
+        final var expectedPrice = BigDecimal.TEN;
+        final var expectedId = "123";
+        final var aRequest = new UpdateProductRequest(expectedName, expectedDescription, expectedPrice, CategoryID.unique().getValue());
+
+        when(updateProductUseCase.execute(any())).thenReturn(UpdateProductOutput.from(expectedId));
+
+        final var request = put("/products/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(aRequest));
+
+        final var response = mvc.perform(request).andDo(print());
+
+        response.andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON.toString()))
+                .andExpect(jsonPath("$.id", equalTo(expectedId)));
+
+        final var captor = ArgumentCaptor.forClass(UpdateProductCommand.class);
+
+        verify(updateProductUseCase).execute(captor.capture());
 
         final var aCmd = captor.getValue();
 
